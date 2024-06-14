ebf2784ad9daa48c1e7a106d9ed922c8bb2b9f0b
Matheus <mateusmasi@gmail.com>
2024-06-14 13:47:07 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Helmet } from "react-helmet";
-import App from "./App"; // Assuming you have an App component
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
-
-const Index = () => (
-  <>
-    <Helmet>
-      <title>Fernanda Gontijo</title>
-    </Helmet>
-    <App />
-  </>
-);
-
-ReactDOM.render(<Index />, document.getElementById("root"));

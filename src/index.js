26f54f28e70c1987b841b18b37340dff8f7cc720
Matheus <mateusmasi@gmail.com>
2024-06-14 13:38:40 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import React, { useEffect } from 'react';
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+
+function App() {
+  useEffect(() => {
+    document.title = "Fernanda Gontijo"; // Set the document title here
+  }, []);
+
+  return (
+    <div>
+      <!-- Your component code here -->
+    </div>
+  );
+}
